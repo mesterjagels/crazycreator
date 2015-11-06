39768809bd4fe2a6e33224b109063a54fad691fc
Jasper <jvve@itu.dk>
2015-11-06 19:59:55 +0100

    no message

---
+++ CrazyCreator/Assets/deathTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathTrigger : MonoBehaviour {
+	
+	public GameObject killer;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*void OnCollisionEnter(Collision col){
+		Debug.Log ("Is colliding");
+
+		if (GameObject.FindWithTag("player")) {
+			deathAnimation ();
+		}
+	}*/
+
+	void deathAnimation(){
+		killer.GetComponent<Rigidbody> ().useGravity = true;
+	}
+	void OnTriggerEnter(Collider other) {
+		print ("something is inside me");
+	}
+}
+
